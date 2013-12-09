0f5ca1754f5ee98e99ab3fd9c48ee517b3551cf5
unknown <arjo0803@ECC102CLAB06.campus.stcloudstate.edu>
2013-12-09 15:29:17 -0600

    updates

--- Member.java
+++ Member.java
@@ -10,95 +10,24 @@ import java.util.*;
 import java.text.*;
 
 
-class Member {
+public class Member extends Person {
 	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	private final static int ACTIVE = 1;
 	private final static int SUSPENDED = 2;
-	private String name;
-	private int id;
-	private String streetAddress;
-	private String city;
-	private String state;
-	private int zipCode;
 	private int active;
 	
-	public Member(){
-		name = null;
-		id = 0;
-		streetAddress = null;
-		city = null;
-		state = null;
-		zipCode = 0;
+	public Member() {
+		super();
 		active = 0;
-	}
-	
-	//This method returns the name of a member
-	public String getName(){
-	  return name;
-	}
-
-	//This method sets the name of a member
-	public void setName(String name){
-	  this.name = name;
-	}
-
-	//This method returns the id of a member
-	public int getID(){
-	  return id;
-	}
-
-	//This method sets the id of a member
-	public void setID(int id){
-	  this.id = id;
-	}
-
-	//This method returns the street address of a member
-	public String getStreetAddress(){
-	  return streetAddress;
-	}
-
-	//This method sets the street address of a member
-	public void setStreetAddress(String streetAddress){
-	  this.streetAddress = streetAddress;
-	}
-
-	//This method returns the city of a member
-	public String getCity(){
-	  return city;
-	}
-
-	//This method sets the city of a member
-	public void setCity(String city){
-	  this.city = city;
-	}
-
-	//This method returns the state of a member
-	public String getState(){
-	  return state;
-	}
-
-	//This method sets the state of a member
-	public void setState(String state){
-	 this.state = state;
-	}
-
-	//This method returns the zipcode of a member
-	public int getZipCode(){
-	  return zipCode;
-	}
-
-	//This method sets the zipcode of a member
-	public void setZipCode(int zipCode){
-	  this.zipCode = zipCode;
 	}
 
 	//This method gets the status of a member
-	public int getStatus(){
+	public int getStatus() {
 	  return active;
 	}
 
 	//This method sets the status of a member
-	public void setStatus(int active){
+	public void setStatus(int active) {
 	  this.active = active;
 	}
 
@@ -116,7 +45,7 @@ class Member {
 	*/
 	
 	//This method writes member data to a file.
-	public void save(){
+	public void save() {
 		String file_name = id + ".txt";
 		try {
 			File file = new File(file_name);
@@ -163,35 +92,35 @@ class Member {
 			//Read File Line By Line
 			while ((strLine = br.readLine()) != null) {
 			  // Print the content on the console
-				if (count == 0){
-					setName(strLine);
-					count++;
-				}
-				else if (count == 1) {
-				    try{
-						// the String to int conversion happens here
-						int i = Integer.parseInt(strLine.trim());
-						setID(i);
-					}
-					catch (NumberFormatException nfe){
-						System.out.println("NumberFormatException: " + nfe.getMessage());
-					}
-					count++;
-				}
-				else if (count == 2){
-				    setStreetAddress(strLine);
-					count++;
-				}
-				else if (count == 3){
-				    setCity(strLine);
-					count++;
-				}
-				else if (count == 4){
-				    setState(strLine);
-					count++;
-				}
-				else if (count == 5){
-				    try{
+        if (count == 0){
+          setName(strLine);
+          count++;
+        }
+        else if (count == 1) {
+          try{
+            // the String to int conversion happens here
+            int i = Integer.parseInt(strLine.trim());
+            setID(i);
+          }
+          catch (NumberFormatException nfe){
+            System.out.println("NumberFormatException: " + nfe.getMessage());
+          }
+          count++;
+        }
+        else if (count == 2) {
+          setStreetAddress(strLine);
+          count++;
+        }
+        else if (count == 3) {
+          setCity(strLine);
+          count++;
+        }
+        else if (count == 4) {
+          setState(strLine);
+          count++;
+        }
+				else if (count == 5) {
+				  try{
 						// the String to int conversion happens here
 						int i = Integer.parseInt(strLine.trim());
 						setZipCode(i);
@@ -201,8 +130,8 @@ class Member {
 					}
 					count++;
 				}
-				else if (count == 6){
-				    try{
+				else if (count == 6) {
+				  try{
 						// the String to int conversion happens here
 						int i = Integer.parseInt(strLine.trim());
 						setStatus(i);
@@ -212,39 +141,39 @@ class Member {
 					}
 					count++;
 				}  
-		}
-		//Close the input stream
-		br.close();
-		}catch (Exception e){//Catch exception if any
+      }
+      //Close the input stream
+      br.close();
+		} catch (Exception e) {//Catch exception if any
 		  System.err.println("Error: " + e.getMessage());
 		}
 	}
 
 	public String getToken(String prompt) {
     do {
-		try {
-			System.out.println(prompt);
-			String line = reader.readLine();
-			StringTokenizer tokenizer = new StringTokenizer(line,"\n\r\f");
-			if (tokenizer.hasMoreTokens()) {
-			  return tokenizer.nextToken();
-			}
-		  } catch (IOException ioe) {
-			System.exit(0);
-		  }
-		} while (true);
+      try {
+        System.out.println(prompt);
+        String line = reader.readLine();
+        StringTokenizer tokenizer = new StringTokenizer(line,"\n\r\f");
+        if (tokenizer.hasMoreTokens()) {
+          return tokenizer.nextToken();
+        }
+      } catch (IOException ioe) {
+          System.exit(0);
+      }
+    } while (true);
 	}
 	
 	public int getNumber(String prompt) {
-		do {
-		  try {
-			String item = getToken(prompt);
-			Integer num = Integer.valueOf(item);
-			return num.intValue();
-		  } catch (NumberFormatException nfe) {
-			System.out.println("Please input a number ");
-		  }
-		} while (true);
+    do {
+      try {
+        String item = getToken(prompt);
+        Integer num = Integer.valueOf(item);
+        return num.intValue();
+      } catch (NumberFormatException nfe) {
+        System.out.println("Please input a number ");
+      }
+    } while (true);
 	}
 	
 
@@ -271,23 +200,16 @@ class Member {
 		String file_name = id + ".txt";
 		Member member = new Member();
 		File f = new File(file_name);
-		if (f.exists()){
+		if (f.exists()) {
 			boolean success = f.delete();
-			if (success){
+			if (success) {
 				System.out.println("Deleted: " + file_name);
 			}
 			return 0;
-		}
-		if (!f.exists()){
+		} else {
 			System.out.println("No such member: " + file_name);
 			return 1;
 		}
 		return 0;
 	}
-
-
-
-
-
-
 }
