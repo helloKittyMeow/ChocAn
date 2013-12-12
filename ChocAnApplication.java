a3e93180c3ea0e81c7dbd7d193d9d2d3a3a7d2a1
unknown <tsts1101@ECC102CLAB02.campus.stcloudstate.edu>
2013-12-12 13:37:31 -0600

    update

--- ChocAnApplication.java
+++ ChocAnApplication.java
@@ -1,15 +1,60 @@
+import java.io.*;
 import java.util.*;
+import java.text.*;
 
 public class ChocAnApplication {
-/*
-  public static String verifyMember() {
+  private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
   
+  public static void verifyMember() {
+    Member member = new Member();
+    int ID = getNumber("Enter the member ID: ", 1, 9999999);
+    
+    member.load(ID);
+    int status = member.getStatus();	
+
+    if(status == 1) {
+      System.out.println("Valid.");
+    }
+    else if(status == 2) {
+      System.out.println("Suspended.");
+    }
   }
   
+/*  
   public static void initializeApplication() {
   
   }
 */
+
+  public static String getToken(String prompt) {
+    while (true) {
+      try {
+        System.out.print(prompt);
+        String line = reader.readLine();
+        StringTokenizer tokenizer = new StringTokenizer(line,"\n\r\f");
+        if (tokenizer.hasMoreTokens()) {
+          return tokenizer.nextToken();
+        }
+      } catch (IOException ioe) {
+        System.exit(0);
+      }
+    }
+  }
+
+  public static int getNumber(String prompt, int MIN, int MAX) {
+    while (true) {
+      try {
+        String item = getToken(prompt);
+        Integer num = Integer.valueOf(item);
+        if ((num.intValue() >= MIN) && (num.intValue() <= MAX)) {
+          return num.intValue();
+        }
+      } catch (NumberFormatException nfe) {
+        System.out.println("Please input a number ");
+      }
+    }
+  }
+  
   public static void main(String[] args) {
   //  UserInterface userInterface = new UserInterface();
     UserInterface.displayMainMenu();
