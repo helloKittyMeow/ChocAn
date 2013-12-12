9fe8658bfaad539f0cf4c95d0f48fc2f586b98ad
unknown <tsts1101@ECC102CLAB02.campus.stcloudstate.edu>
2013-12-12 14:10:32 -0600

    update

--- ComputeWeeklyReport.java
+++ ComputeWeeklyReport.java
@@ -4,60 +4,36 @@ import java.text.SimpleDateFormat;
 import java.lang.*;
 
 public class ComputeWeeklyReport {
-  //Provider provider = new Provider();
- // Member member = new Member();
   
   public static void compute() {
     int memberCount = 0;
     int providerCount = 0;
-    int totalFee = 0;
+    double providerFeeTotal = 0;
     int id = 0;
-    File folder = new File("X:/Desktop/ChocAn");
+    File folder = new File("./");
     File[] listOfFiles = folder.listFiles();
+    File folder2 = new File("./EFTReports");
+    File[] listOfFiles2 = folder2.listFiles();
 
     for (File file : listOfFiles) {
         if (file.isFile()) {
-            //System.out.println(file.getName());
-            if(file.getName().startsWith("m")){
+            if(file.getName().startsWith("m")){           
               id = Person.getFileID(file.getName());
-              ProviderReport.printReport(id);
+              MemberReport.printReport(id); 
               memberCount++;
             }
             if(file.getName().startsWith("p")){
               id = Person.getFileID(file.getName());
-              MemberReport.printReport(id);
+              ProviderReport.printReport(id);
               providerCount++;
-            }
+            }           
+            EFTReport.printReport();
         }
     } 
     System.out.println(memberCount + " members in the directory.");
     System.out.println(providerCount + " providers in the directory.");
- 
- 
- /* 
-    //open directory with providers (holds services provided)
-    while (more providers exist) {
-      provider = next provider in file
-      printProviderReport(provider.id) 
-   } 
-   
-
-    while (more members exist) {
-      members = next members in file
-      printManagerReport(members.id)
-    }
-
-    while (more EFT reports exist) {
-      eft = next eft in file
-      printEFTReport(eft.name)
-      fee += amount to be transferred
-    }
-
-    print number of members
-    print fee
-    */
+    System.out.println("Provider Fee Total: " + providerFeeTotal);
   }
-  
 }
 
 
